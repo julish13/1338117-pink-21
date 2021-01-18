3044caeddc5eb19c27327978bc8a7c6730ed95df
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-01-18 13:54:45 +0300

    fix: index-reviews mobile layout and js

--- source/js/reviews.js
+++ source/js/reviews.js
@@ -2,10 +2,16 @@
 
 const reviews = document.querySelector(`.reviews`);
 const reviewsList = reviews.querySelector(`.reviews__list`);
+const reviewsSliders = reviews.querySelectorAll(`.slider__slide`);
 const reviewsToggles = reviews.querySelector(`.slider__toggles`);
 const reviewsOptions = reviews.querySelector(`.slider__options`);
 const leftArrow = reviewsOptions.querySelector(`.slider__arrow--left`);
 const rightArrow = reviewsOptions.querySelector(`.slider__arrow--right`);
+
+for (const slide of reviewsSliders) {
+  slide.classList.remove(`slider__slide--nojs`);
+  slide.classList.add(`slider__slide--js`);
+}
 
 createDotsSlider(changeSlide, reviewsToggles, reviewsList);
 createArrowsSlider(leftArrow, `left`, reviewsList);
