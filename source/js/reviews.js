0ecb3dd3ae8cf51d1e5d096ba19d50ecbfe59a35
Yulia Kolupaeva <jkolupaeva@gmail.com>
2021-01-20 16:14:46 +0300

    fix js reviews-toggle issue

--- source/js/reviews.js
+++ source/js/reviews.js
@@ -1,22 +1,22 @@
 "use strict";
 
-const reviews = document.querySelector(`.reviews`);
-const reviewsList = reviews.querySelector(`.reviews__list`);
-const reviewsSliders = reviews.querySelectorAll(`.slider__slide`);
-const reviewsToggles = reviews.querySelectorAll(`.slider__toggles`);
-const reviewsOptions = reviews.querySelector(`.slider__options`);
-const leftArrow = reviewsOptions.querySelector(`.slider__arrow--left`);
-const rightArrow = reviewsOptions.querySelector(`.slider__arrow--right`);
+  const reviews = document.querySelector(`.reviews`);
+  const reviewsList = reviews.querySelector(`.reviews__list`);
+  const reviewsSliders = reviews.querySelectorAll(`.slider__slide`);
+  const reviewsToggles = reviews.querySelector(`.slider__toggles`);
+  const reviewsOptions = reviews.querySelector(`.slider__options`);
+  const leftArrow = reviewsOptions.querySelector(`.slider__arrow--left`);
+  const rightArrow = reviewsOptions.querySelector(`.slider__arrow--right`);
 
-for (const slide of reviewsSliders) {
-  slide.classList.remove(`slider__slide--nojs`);
-  slide.classList.add(`slider__slide--js`);
-}
+  for (const slide of reviewsSliders) {
+    slide.classList.remove(`slider__slide--nojs`);
+    slide.classList.add(`slider__slide--js`);
+  }
 
-reviewsToggles.classList.remove(`slider__toggles--nojs`);
-reviewsToggles.classList.add(`slider__toggles--js`);
+  reviewsToggles.classList.remove(`slider__toggles--nojs`);
+  reviewsToggles.classList.add(`slider__toggles--js`);
 
 
-createDotsSlider(changeSlide, reviewsToggles, reviewsList);
-createArrowsSlider(leftArrow, `left`, reviewsList);
-createArrowsSlider(rightArrow, `right`, reviewsList);
+  createDotsSlider(changeSlide, reviewsToggles, reviewsList);
+  createArrowsSlider(leftArrow, `left`, reviewsList);
+  createArrowsSlider(rightArrow, `right`, reviewsList);
